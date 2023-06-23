6e913c16faf9e2eb7402b09a66ab52598363122c
muhamad sadiq <solinsadiq11@gmail.com>
2023-06-23 13:56:04 +0300

    using PostConstruct annotation note: both the @PostConstruct and @PreDestroy annotations are part of Java EE. Since Java EE was deprecated in Java 9, and removed in Java 11, we have to add an additional dependency to use these annotations:

--- src/main/java/bean_creation/beans/Vehicle.java
+++ src/main/java/bean_creation/beans/Vehicle.java
@@ -3,6 +3,8 @@ package bean_creation.beans;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class Vehicle {
     private String name;
@@ -13,5 +15,12 @@ public class Vehicle {
 
     public void setName(String name) {
         this.name = name;
+    }
+    /*
+    * this method execute after the bean created
+    */
+    @PostConstruct
+    public void initialize(){
+        this.name = "G Class";
     }
 }
